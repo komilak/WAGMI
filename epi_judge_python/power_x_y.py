bd4a272a95834a862bcfd6e87da9b4cb767d4382
Komila K <2komilak@gmail.com>
2024-05-09 14:06:06 -0700

    pow(x,y)

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,8 +2,26 @@ from test_framework import generic_test
 
 
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    # x to the power of y
+    # When y is a power of 2, the multiplications can be iterated by squaring like x, x 2, (x 2 ) 2
+    # When y is nonnegative, if the least significant bit of y is 0, the result is (x y ∕ 2 ) 2
+    # When y is nonnegative, if the least significant bit of y is 1, the result is x × (x y ∕ 2 ) 2
+    # When y is negative, if the least significant bit of y is 0, the result is (1 ∕ x -y ∕ 2 ) 2
+    # When y is negative, if the least significant bit of y is 1, the result is 1 ∕ x × (1 ∕ x -y ∕ 2 ) 2
+    result = 1
+    if y < 0:
+        y = -y
+        x = 1/x
+    # loop until y/power = 0
+    while y:
+        # If the least sig bit of y is 1, compute the result = result × x
+        if y & 1:
+            result *= x
+        # Compute x = x × x and y = y ∕ 2
+        x *= x
+        y >>= 1
+
+    return result
 
 
 if __name__ == '__main__':
