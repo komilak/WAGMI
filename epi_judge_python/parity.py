26af262d150a7808f3dc6c4d99a2bde47fff1264
Komila K <2komilak@gmail.com>
2024-05-08 18:53:29 -0700

    4.1 solution complete

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -3,7 +3,16 @@ from test_framework import generic_test
 
 def parity(x: int) -> int:
     # TODO - you fill in here.
-    return 0
+    result = 0
+
+    # while the number is not yet reduced to 0
+    # loop is skipped if num is 0, parity is 0 by default
+    while x:
+        result ^= 1
+        # x&(x-1) is x with its lowest bit erased
+        x &= x - 1
+        
+    return result
 
 
 if __name__ == '__main__':
